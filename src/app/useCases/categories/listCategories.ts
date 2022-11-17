13d7791955881f0b0227f255ca9059f1d38c05ce
nils0n <nilson_jose.v@hotmail.com>
2022-11-16 21:01:03 -0300

    fix:ajusted spaces

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -4,6 +4,7 @@ import { Category } from '../../models/Category';
 
 export async function lisCategories(req: Request, res: Response) {
   try {
+
     const categories = await Category.find();
     res.json(categories);
 
